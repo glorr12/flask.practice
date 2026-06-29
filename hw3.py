abbdd324377606427aa8bb36387972f5eef85420
glorr12 <igor.fishkov1991@gmail.com>
2026-06-29 15:49:37 +0100

    hw3

--- hw3.py
+++ hw3.py
@@ -22,7 +22,7 @@ class Category(Base):
 
 
 class Product(Base):
-    __tablename__ = 'products'
+    __tablename__ = "products"
 
     id:    Mapped[int] = mapped_column(primary_key=True)
     name:  Mapped[str] = mapped_column(String(100))
