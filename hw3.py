de701731522e48e244fe81052c6a7c3f14bca74b
glorr12 <igor.fishkov1991@gmail.com>
2026-06-29 15:48:58 +0100

    hw2

---
+++ hw3.py
@@ -0,0 +1,36 @@
+import decimal
+from typing import Optional
+from sqlalchemy import String, Boolean, create_engine, ForeignKey,Numeric
+from sqlalchemy.orm import Session, declarative_base, Mapped, mapped_column, DeclarativeBase
+
+engine = create_engine('sqlite:///:memory:')
+
+
+session = Session(bind=engine)
+
+
+class Base(DeclarativeBase):
+    pass
+
+class Category(Base):
+    __tablename__ = 'categories'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(100))
+    description: Mapped[Optional[str]] = mapped_column(String(255))
+
+
+
+class Product(Base):
+    __tablename__ = 'products'
+
+    id:    Mapped[int] = mapped_column(primary_key=True)
+    name:  Mapped[str] = mapped_column(String(100))
+    price: Mapped[decimal.Decimal] = mapped_column(Numeric(10,2))
+    in_stock: Mapped[bool] = mapped_column(Boolean)
+    category_id: Mapped[int] = mapped_column(ForeignKey('categories.id'))
+
+
+
+
+Base.metadata.create_all(engine)
